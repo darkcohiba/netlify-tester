a1ed016c09a56189d2caa3df85d5e3180a0761ff
Sam Waters <samuel.waters@gmail.com>
2023-08-17 22:51:38 -0600

    added joke

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
 console.log("hello")
 
-// function fetchJoke(){
+const jokeButton = document.querySelector("#jokeButton")
 
-// }
+
+
+jokeButton.addEventListener("click", fetchJoke)
+
+
+function fetchJoke(){
+    fetch("https://v2.jokeapi.dev/joke/Any")
+    .then(response => response.json())
+    .then(data => function(){
+        console.log(data)
+        if (data.type === "twopart"){
+            renderTwoPartJoke(data.delivery, data.setup)
+        }else{
+            renderSinglePartJoke(data.joke)
+        }
+
+    }())
+}
+
+function renderTwoPartJoke(delivery, setup){
+    const deliveryText = document.querySelector("#delivery")
+    const setupText = document.querySelector("#setup")
+    const jokeText = document.querySelector("#joke")
+    console.log(delivery)
+    jokeText.textContent = ""
+    deliveryText.textContent = delivery
+    setupText.textContent = setup
+
+
+}
+
+function renderSinglePartJoke(joke){
+    const deliveryText = document.querySelector("#delivery")
+    const setupText = document.querySelector("#setup")
+    const jokeText = document.querySelector("#joke")
+
+    jokeText.textContent = joke
+    deliveryText.textContent = ""
+    setupText.textContent = ""
+
+}
+
